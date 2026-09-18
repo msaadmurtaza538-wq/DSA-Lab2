33e8b1f0af0b626ea5b45e9bf6acd867ffdbcb33
msaadmurtaza538-wq <msaadmurtaza538@gmail.com>
2026-09-18 23:17:29 +0500

    handled an edge case

--- sorting.cpp
+++ sorting.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 bool isSorted(const int *arr, const int size)
 {
-    if (size <= 1) return true;
     for (int i = 0; i < size - 1; i++)
     {
         if (arr[i] > arr[i + 1])
