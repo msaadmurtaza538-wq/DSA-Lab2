0aee61d6bdd24c86d9882d87d14b5da9a3a73d19
msaadmurtaza538-wq <msaadmurtaza538@gmail.com>
2026-09-18 23:15:08 +0500

    implemented isSorted function

--- sorting.cpp
+++ sorting.cpp
@@ -3,6 +3,19 @@
 
 using namespace std;
 
+bool isSorted(const int *arr, const int size)
+{
+    if (size <= 1) return true;
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 void testSortedArray()
 {
     int arr[] = {1, 2, 3, 4, 5};
